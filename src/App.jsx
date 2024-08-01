2a047b29b19bbbb455a2bc6c016dbd4aa6a699c2
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-01 20:17:59 +0000

    adding home section to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import SideNavBar from './Components/SideBar'
+import Home from './Components/Home'
 import "../src/CSS/App.css"
 function App() {
 
   return (
     <>
-      <SideNavBar/>
+      <SideNavBar />
+      <Home />
     </>
   )
 }
