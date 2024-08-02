6311fae3c0f2ab76dfdc8e9d1cb7e3b6502b1b88
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-02 22:19:25 +0000

    making then programing room

--- src/Components/Rooms/Programing.jsx
+++ src/Components/Rooms/Programing.jsx
@@ -1,11 +1,36 @@
-import '../../CSS/Home.css'
-
+import React from "react"
+import "../../CSS/Home.css"
+import "../../CSS/Rooms.css"
+import ProgramingJson from "../../JSON/ProgramingBooks.json"
 const Programing = () => {
+  let books = Object.entries(ProgramingJson.books).map(book => {
+    return (book[1].map(variable => {
+      return (
+        <Book {...variable}></Book>
+      )
+    })
+    )
+  })
   return (
     <div className="home-page-div">
-      <h1 className="title center">Programing</h1>
+      <div className="shelf">
+        {books}
+      </div>
     </div>
   )
 }
+
+class Book extends React.Component {
+  render() {
+    return (
+      <div className={this.props.title ? "title-container" : "book-container"}>
+        {this.props.title && <h1 className="title">{this.props.title}</h1>}
+        {this.props.icon && <img className="book-img" src={this.props.icon} />}
+        {this.props.link && <a href={this.props.link} className="center button">Read</a>}
+      </div>
+    )
+  }
+}
+
 
 export default Programing
