709fe7af8c31a96ddb605757ffb77ba6304884ae
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-02 08:46:18 +0000

    making the website from single page to multi page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
 import SideNavBar from './Components/SideBar'
 import Home from './Components/Home'
 import "../src/CSS/App.css"
+import { Route,Routes } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import Programing from './Components/Rooms/Programing'
+import Math from './Components/Rooms/Math'
+
 function App() {
 
   return (
     <>
-      <SideNavBar />
-      <Home />
+      <Router>
+
+        <SideNavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/math" element={<Math />} />
+          <Route path="programing" element={<Programing />} />
+        </Routes>
+      </Router>
     </>
   )
 }
