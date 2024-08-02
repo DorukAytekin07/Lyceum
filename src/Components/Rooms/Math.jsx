38b5bd98289a3746c27b071de2b24ff92969874f
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-02 16:39:09 +0000

    coding the math room

--- src/Components/Rooms/Math.jsx
+++ src/Components/Rooms/Math.jsx
@@ -1,11 +1,36 @@
+import React from "react"
 import "../../CSS/Home.css"
-
+import "../../CSS/Rooms.css"
+import MathJson from "../../JSON/MathBooks.json"
 const Math = () => {
+  let books = Object.entries(MathJson.books).map(book => {
+    return (book[1].map(variable => {
+      return (
+        <Book {...variable}></Book>
+      )
+    })
+    )
+  })
   return (
     <div className="home-page-div">
-      <h1 className="title center" >Math</h1>
+      <div className="shelf">
+        {books}
+      </div>
     </div>
   )
 }
+
+class Book extends React.Component {
+  render() {
+    return (
+      <div className={this.props.title ? "title-container" : "book-container"}>
+        {this.props.title && <h1 className="title">{this.props.title}</h1>}
+        {this.props.icon && <img className="img-container" src={this.props.icon} />}
+        {this.props.link && <a href={this.props.link} className="center button">Read</a>}
+      </div>
+    )
+  }
+}
+
 
 export default Math
