4e336dae77e5406915430f5561919ed3c68b81ff
Doruk Aytekin <dorukaytekinexxon@gmail.com>
2024-08-02 08:45:43 +0000

    making the rooms jsx files

---
+++ src/Components/Rooms/Math.jsx
@@ -0,0 +1,11 @@
+import "../../CSS/Home.css"
+
+const Math = () => {
+  return (
+    <div className="home-page-div">
+      <h1 className="title center" >Math</h1>
+    </div>
+  )
+}
+
+export default Math
---
+++ src/Components/Rooms/Programing.jsx
@@ -0,0 +1,11 @@
+import '../../CSS/Home.css'
+
+const Programing = () => {
+  return (
+    <div className="home-page-div">
+      <h1 className="title center">Programing</h1>
+    </div>
+  )
+}
+
+export default Programing
